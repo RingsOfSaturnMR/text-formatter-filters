27ab8808baa970ff968ed714bd68fda460438aab
Matt <RingsOfSaturnMR@users.noreply.github.com>
2019-03-29 01:35:50 -0400

    Fixed bug that let copy and paste get bad input through.

--- textformatterfilters/SingleDigitFilter.java
+++ textformatterfilters/SingleDigitFilter.java
@@ -17,8 +17,7 @@ https://github.com/karimsqualli96
 from his gist at
 https://gist.github.com/karimsqualli96
 "DoubleFormattedTextField.java"
-*/
-
+ */
 /**
  *
  * @author mattroberts
@@ -29,13 +28,19 @@ public class SingleDigitFilter implements UnaryOperator<TextFormatter.Change> {
     public TextFormatter.Change apply(TextFormatter.Change textFormatter) {
 
         if (textFormatter.isReplaced()) {
-            if (!Character.isDigit(textFormatter.getControlNewText().charAt(0))) {
-                textFormatter.setText("");
-            } else {
+            if (Character.isDigit(textFormatter.getControlNewText().charAt(0))) {
                 textFormatter.setText(Character.toString(textFormatter.getControlNewText().charAt(0)));
+            } else if (textFormatter.getControlText().length() > 0) {
+                textFormatter.setText(textFormatter.getControlText());
+            } else {
+                textFormatter.setText("");
             }
         } else if (textFormatter.isAdded()) {
-            if (textFormatter.getControlText().length() > 0 || (!Character.isDigit(textFormatter.getControlNewText().charAt(0)))){
+            if (textFormatter.getControlText().length() > 0) {
+                textFormatter.setText("");
+            } else if (Character.isDigit(textFormatter.getControlNewText().charAt(0))) {
+                textFormatter.setText(Character.toString(textFormatter.getControlNewText().charAt(0)));
+            } else {
                 textFormatter.setText("");
             }
         }
